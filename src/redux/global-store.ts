0c794c7d18604771e1abee970f7c3292a51c4154
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-03-06 22:49:16 -0500

    add root state

--- src/redux/global-store.ts
+++ src/redux/global-store.ts
@@ -1,6 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { UserObject } from "../types/user-types";
+import { StepOneState } from "../types/create-listing-form.types";
+
 import userReducer from "./user-slice";
 import stepOneFormReducer from "./step-one-slice";
+
+export interface RootState {
+  user: UserObject;
+  stepOneForm: StepOneState;
+}
 
 export default configureStore({
   reducer: { user: userReducer, stepOneForm: stepOneFormReducer },

165d71fc6dae68b99ad5e87f8c2c006fb9f3ba53
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-03-16 22:49:33 -0400

    added empty navbar to desktop

--- src/components/navbar/navbar-header.desktop.tsx
+++ src/components/navbar/navbar-header.desktop.tsx
@@ -1,7 +1,32 @@
+import { AppBar, Box, IconButton, Toolbar } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import logo from "/vite.svg";
+
 const NavbarHeaderDesktopLayout = () => {
   return (
-    <div>navbar-header.DesktopLayout</div>
-  )
-}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar
+          sx={{
+            justifyContent: "space-between",
+          }}
+        >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <img src={logo} alt="" />
+          </IconButton>
+          <IconButton color="inherit">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
-export default NavbarHeaderDesktopLayout
+export default NavbarHeaderDesktopLayout;

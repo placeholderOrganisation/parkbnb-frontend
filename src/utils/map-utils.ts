4114c3f5ea691c3291a37deff8076eedbc28130c
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-04-28 21:32:35 -0400

    updated defaut zoom

--- src/utils/map-utils.ts
+++ src/utils/map-utils.ts
@@ -1,38 +1,40 @@
+const defaultZoom = 9;
+const cityZoom = 10;
 export const cityToCoords = {
   default: {
     lat: 43.708266,
     lng: -79.60997,
-    zoom: 10,
+    zoom: defaultZoom,
   },
   brampton: {
     lat: 43.7315,
     lng: -79.7624,
-    zoom: 11,
+    zoom: cityZoom,
   },
   mississauga: {
     lat: 43.589,
     lng: -79.6441,
-    zoom: 11,
+    zoom: cityZoom,
   },
   toronto: {
     lat: 43.65107,
     lng: -79.347015,
-    zoom: 11,
+    zoom: cityZoom,
   },
   vaughan: {
     lat: 43.8361,
     lng: -79.4982,
-    zoom: 11,
+    zoom: cityZoom,
   },
   markham: {
     lat: 43.8561,
     lng: -79.337,
-    zoom: 11,
+    zoom: cityZoom,
   },
   oakville: {
     lat: 43.4675,
     lng: -79.6877,
-    zoom: 11,
+    zoom: cityZoom,
   },
 };
 

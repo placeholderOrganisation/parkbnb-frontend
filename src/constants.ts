6341ee4edaa8f09b30e3be5f3bcab198f89a68ad
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-05-10 13:52:03 -0400

    change company name

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-export const parkingAppDomain = "http://209.38.8.8/";
+export const parkingAppDomain = "rentaparking.ca";
 
-export const COMPANY_NAME = "Parking Now";
+export const COMPANY_NAME = "Rent a parking";
 
-export const shareableMessaBody = 'Check out this parking spot on Parking Now!'
+export const shareableMessaBody = `Check out this parking spot on ${COMPANY_NAME}!`
 
 export const interestMessageBody = `Hi I saw your parking on ${parkingAppDomain} I am interested in renting it! Is it available? Please let me know. Thanks!`

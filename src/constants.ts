d277231d3e2f433addbfe0f8ca406aae6bbaa507
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-05-09 22:59:09 -0400

    update the interest message being sent

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
+export const parkingAppDomain = "http://209.38.8.8/";
+
 export const COMPANY_NAME = "Parking Now";
 
 export const shareableMessaBody = 'Check out this parking spot on Parking Now!'
 
-export const interestMessageBody = 'I am interested in renting this parking spot! Is it available? If not, please mark it as rented / delete the listing on'
+export const interestMessageBody = `Hi I saw your parking on ${parkingAppDomain} I am interested in renting it! Is it available? Please let me know. Thanks!`

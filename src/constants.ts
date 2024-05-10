e8398c30a282b2a93e7e250e321dceb82b2fcf15
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-05-10 13:57:36 -0400

    update interest message body

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,4 @@ export const COMPANY_NAME = "Rent a parking";
 
 export const shareableMessaBody = `Check out this parking spot on ${COMPANY_NAME}!`
 
-export const interestMessageBody = `Hi I saw your parking on ${parkingAppDomain} I am interested in renting it! Is it available? Please let me know. Thanks!`
+export const interestMessageBody = `Hi I saw your parking on ${parkingAppDomain} I am interested in renting it! Please let me know if it is available. Thanks!`
